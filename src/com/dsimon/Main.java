0297b34f4c8abfa0a3826c7cccd57ca4639e4685
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-20 23:31:06 -0400

    exercise complete

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,15 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(NumberOfDaysInMonth.isLeapYear(-1600));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2017));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2000));
+
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2018));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2020));
     }
 }
